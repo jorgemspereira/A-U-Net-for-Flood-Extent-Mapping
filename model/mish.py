67355b3647f7412e951dc40542966f298f43515c
Bruno Martins <bgmartins@users.noreply.github.com>
2019-10-15 19:51:16 +0100

    Create mish.py
    Added mish activation function.

---
+++ model/mish.py
@@ -0,0 +1,26 @@
+# Keras Implementation of Mish Activation Function.
+
+from keras.layers import Activation
+from keras.utils.generic_utils import get_custom_objects
+import keras.backend as K
+
+class Mish(Activation):
+    '''
+    Mish Activation Function.
+    .. math::
+        mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + e^{x}))
+    Shape:
+        - Input: Arbitrary. Use the keyword argument `input_shape`
+        (tuple of integers, does not include the samples axis)
+        when using this layer as the first layer in a model.
+        - Output: Same shape as the input.
+    Examples:
+        >>> X = Activation('Mish', name="conv1_act")(X_input)
+    '''
+    def __init__(self, activation, **kwargs):
+        super(Mish, self).__init__(activation, **kwargs)
+        self.__name__ = 'Mish'
+
+def mish(x): return x*K.tanh(K.softplus(x))
+
+get_custom_objects().update({'Mish': Mish(mish)})
